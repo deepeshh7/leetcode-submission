b0df1f30fdd859a5c7b41a6363923c12afde218d
Deepesh Padhy <deepeshpadhy0247@gmail.com>
2026-07-08 12:24:01 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+
+        unordered_map<char,int> scount;
+        unordered_map<char,int> tcount;
+
+        for(int i=0;i<s.length();i++)
+        {
+            scount[s[i]]++;
+            tcount[t[i]]++;
+        }
+        if(scount==tcount) return true;
+        else return false;
+    }
+};

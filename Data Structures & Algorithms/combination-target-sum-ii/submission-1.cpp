82696950c0f9de981cd71e8eaf051aef0173e2ed
Deepesh Padhy <deepeshpadhy0247@gmail.com>
2026-09-02 17:57:40 +0530

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<int> cur;
+        backtrack(nums, target, cur, 0);
+        return res;
+    }
+private:
+    void backtrack(vector<int>& nums, int target,vector<int>& cur, int i)
+    {
+        if(target==0)
+        {
+            res.push_back(cur);
+            return;
+        }
+        if(target<0 || i>=nums.size()) return;
+
+        cur.push_back(nums[i]);
+        backtrack(nums, target-nums[i],cur, i+1);
+        cur.pop_back();
+        backtrack(nums, target, cur, i+1);
+    }
+};
